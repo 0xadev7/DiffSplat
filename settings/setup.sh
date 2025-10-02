0f020c3d8e004e26ef9a648a25c4159920c966cb
0xadev7 <leric7510@gmail.com>
2025-10-02 11:05:28 -0400

    fix setup

--- settings/setup.sh
+++ settings/setup.sh
@@ -1,8 +1,12 @@
 PROJECT_DIR=$(pwd)
 
-# Pytorch
-pip3 install -i https://download.pytorch.org/whl/cu124 -U torch==2.4.1 torchvision==0.19.1 torchaudio==2.4.1
-pip3 install -i https://download.pytorch.org/whl/cu124 -U xformers==0.0.29.post3
+# install the PyTorch trio from the cu124 index
+pip install --index-url https://download.pytorch.org/whl/cu124 \
+  torch==2.6.0+cu124 torchvision==0.21.0+cu124 torchaudio==2.6.0+cu124
+
+# install xformers built against torch 2.6 + cu124
+pip install --extra-index-url https://download.pytorch.org/whl/cu124 \
+  xformers==0.0.29.post3
 
 # A modified gaussian splatting (+ alpha, depth, normal rendering)
 cd extensions && git clone https://github.com/BaowenZ/RaDe-GS.git --recursive && cd RaDe-GS/submodules
