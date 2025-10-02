76c9f53d35ab9915c959feb27b5a04d0f669f6bd
0xadev7 <leric7510@gmail.com>
2025-10-02 10:49:12 -0400

    fix: install

--- settings/setup.sh
+++ settings/setup.sh
@@ -1,8 +1,8 @@
 PROJECT_DIR=$(pwd)
 
 # Pytorch
-pip3 install -i https://download.pytorch.org/whl/cu121 -U torch==2.3.1 torchvision==0.18.1 torchaudio==2.3.1
-pip3 install -i https://download.pytorch.org/whl/cu121 -U xformers==0.0.27
+pip3 install -i https://download.pytorch.org/whl/cu124 -U torch==2.3.1 torchvision==0.18.1 torchaudio==2.3.1
+pip3 install -i https://download.pytorch.org/whl/cu124 -U xformers==0.0.27
 
 # A modified gaussian splatting (+ alpha, depth, normal rendering)
 cd extensions && git clone https://github.com/BaowenZ/RaDe-GS.git --recursive && cd RaDe-GS/submodules
@@ -12,4 +12,4 @@ cd ${PROJECT_DIR}
 # Others
 pip3 install -U gpustat
 pip3 install -U -r settings/requirements.txt
-sudo apt-get install -y ffmpeg
+apt-get install -y ffmpeg
